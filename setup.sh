b2a5ec55e0e78d735383c67400cf2ed1c201da50
Seungwoo Kim <seunkim@c2r1s5.42seoul.kr>
2020-08-03 23:06:25 +0900

    ftps ok but diff external ip

--- setup.sh
+++ setup.sh
@@ -1,14 +1,21 @@
 echo "docker-env"
 eval $(minikube docker-env)
 
-echo "nginx image build..."
-docker build -t ft_nginx ./srcs/nginx
-
-echo "metalLB manifest"
+echo "metalLB apply"
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl create -f ./srcs/metallb/config.yaml
 
+IP=$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)
+printf "Minikube IP: ${IP}"
+
+echo "nginx image build..."
+docker build -t ft_nginx ./srcs/nginx
+echo "ftps image build..."
+docker build -t ft_ftps ./srcs/ftps --build-arg IP=${IP}
+
 echo "nginx service create..."
-kubectl apply -f ./srcs/nginx.yaml
+kubectl apply -f ./srcs//yaml/nginx.yaml
+echo "ftps service create..."
+kubectl apply -f ./srcs/yaml/ftps.yaml
---
+++ srcs/ftps/start.sh
@@ -0,0 +1,1 @@
+pure-ftpd -j -Y 2 -p 30020:30020 -p 30021:30021 -P $(cat ip.txt) 
